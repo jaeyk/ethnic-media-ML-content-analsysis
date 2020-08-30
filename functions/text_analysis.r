6f2b7c254001321d3cfff7f13c41a5300848130f
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2020-08-30 00:09:18 -0700

    finalized word freq

--- functions/text_analysis.r
+++ functions/text_analysis.r
@@ -1,19 +1,27 @@
-visualize_wf <- function(data){
+visualize_wf <- function(data, custom_title){
   
-  data %>%
-  ggplot(aes(x = Collective_gain, y = Collective_loss)) +
-    geom_jitter(alpha = 0.1, size = 2.5, width = 0.25, height = 0.25) +
-    ggrepel::geom_text_repel(
-              aes(label = bigram), 
-              hjust = 0.5, # for center alignment 
-              size = 2) +
-    scale_x_log10(labels = scales::percent_format()) +
-    scale_y_log10(labels = scales::percent_format()) +
-    geom_abline(color = "red") +
-    facet_wrap(~group) +
-    labs(x = "Collective gain",
-         y = "Collective loss")
-  
+asian <- data %>%
+  filter(group == "Asian Americans") %>%
+  mutate(diff = Collective_gain - Collective_loss) %>%
+  ggplot(aes(x = fct_reorder(bigram, diff), y = diff)) +
+  geom_col() +
+  coord_flip() +
+  labs(title = {{custom_title}},
+       subtitle = "Asian Americans",
+       x = "Bigrams",
+       y = "# of Collective Gain - # of Collective Loss") 
+
+black <- data %>%
+  filter(group != "Asian Americans") %>%
+  mutate(diff = Collective_gain - Collective_loss) %>%
+  ggplot(aes(x = fct_reorder(bigram, diff), y = diff)) +
+  geom_col() +
+  coord_flip() +
+  labs(subtitle = "African Americans",
+     x = "Bigrams",
+     y = "# of Collective Gain - # of Collective Loss")
+
+asian / black
 }
 
 # The following tidy_text function heavily draws on https://www.tidytextmining.com/ngrams.html
@@ -41,58 +49,66 @@ tidy_text <- function(data, var1, var2){
   # Remove stop words; This part of code comes from Mhairi McNeill:https://stackoverflow.com/a/37526926
   
   stopwords <- paste(tm::stopwords('en'), collapse = "\\b|\\b")
-
+  
   stopwords_regex <- paste0("\\b", stopwords, "\\b")
   
   data$text <- gsub(stopwords_regex, "", data$text)
   
-  data
+  data 
   
-  }
-
-tokenize_text <- function(data){
-
-data %>%
-    # tokenize
-    unnest_tokens(bigram, text, token = "ngrams", n = 2)
-
 }
 
-create_word_frequency <- function(data, asian_thres, black_thres){
+tokenize_text <- function(data){
   
-  df <- data %>%
+  data %>%
+    # tokenize
+    unnest_tokens(bigram, text, token = "ngrams", n = 2)
+  
+}
+
+create_word_frequency <- function(data){
+  
+  data %>%
     group_by(linked_fate, group) %>%
     count(bigram, sort = TRUE) %>%
-    # Subjoin
-    left_join(data %>%
-                group_by(linked_fate, group) %>%
-                summarise(total = n())) %>%
-    # Create freq variable
-    mutate(freq = n/total) %>%
-    mutate(etc = as.numeric(str_detect(bigram, "year|years|people|san|francisco|oakland|washington|american|americans|th|street|month|months|week|weeks|western|california|united|los|international|district|hong|west|pacific|first|second|new york|per cent|alameda|county|bay area|mr|dont know|will|sun reporter|east bay|men women|contra costa|make sure|can get|berkeley|early|community|park|junior|student|dont want|university|task force|association|chinese|man"))) %>%
-    filter(etc != 1)
+    # Select only interested columns
+    select(linked_fate, group, bigram, n)
   
-  message(paste("subjoin and filter done"))
-  
-  asian <- df %>%
-    filter(group == "Asian Americans") %>%
-    arrange(desc(freq)) %>%
-    top_n(asian_thres) 
+}
 
-  message(paste("asian part done"))
+
+filter_n <- function(data){
   
-  black <- df %>%
-    filter(group != "Asian Americans") %>%
-    arrange(desc(freq)) %>%
-    top_n(asian_thres) 
+  asian_lp <- data %>% filter(group == "Asian Americans", linked_fate == "Collective_gain") 
+
+  asian_lh <- data %>% filter(group == "Asian Americans", linked_fate != "Collective_gain") 
+
+  asian_lp <- asian_lp %>% arrange(desc(n)) %>% head(15) 
   
-  message(paste("black part done"))
+  asian_lh <- asian_lh %>% arrange(desc(n)) %>% head(15) 
+  
+  black_lp <- data %>% filter(group != "Asian Americans", linked_fate == "Collective_gain") 
+
+  black_lh <- data %>% filter(group != "Asian Americans", linked_fate != "Collective_gain") 
+  
+  black_lp <- black_lp %>% arrange(desc(n)) %>% head(15) 
+  
+  black_lh <- black_lh %>% arrange(desc(n)) %>% head(15)
+  
+  asian <- bind_rows(asian_lp, asian_lh) 
+  black <- bind_rows(black_lp, black_lh) 
   
   bind_rows(asian, black) %>%
-    select(linked_fate, group, bigram, freq) %>%
-    pivot_wider(names_from = c("linked_fate"),
-                values_from = "freq") %>%
-    arrange("Collective_gain", "Collective_loss") 
+    spread("linked_fate", "n") %>%
+    replace(is.na(.), 0)
+  
+}
+
+filter_words <- function(data){
+  
+  data %>%
+    mutate(etc = as.numeric(str_detect(bigram, "year|years|san|francisco|oakland|washington|th|street|month|months|week|weeks|western|california|united|los|international|district|hong|west|first|second|new york|per cent|alameda|county|bay area|mr|dont know|will|sun reporter|east bay|men women|contra costa|make sure|can get|berkeley|early|community|park|junior|student|dont want|university|task force|association|man|american|americans|pacific|asian|chinese|black|school"))) %>%
+    filter(etc != 1)
   
 }
 
